b51dcdbc93fee115d8999f5cef3f42cb16625387
Ana Juarez <ajuarez@ex2outcoding.local>
2018-11-22 23:10:30 -0600

    Finca

--- src/main/java/com/cenfotec/exam/domain/Finca.java
+++ src/main/java/com/cenfotec/exam/domain/Finca.java
@@ -1,5 +1,44 @@
 package com.cenfotec.exam.domain;
 
+import org.springframework.data.annotation.Id;
+
 public class Finca {
+	@Id
+	private String Id;
+	private String Nombre;
+	private String Descripcion;
+	
+	//Constructors
+	public Finca() {}
+	public Finca(String id, String nombre, String descripcion) {
+		Id = id;
+		Nombre = nombre;
+		Descripcion = descripcion;
+	}
+
+	// Getters & Setters
+	public String getId() {
+		return Id;
+	}
+	
+	public void setId(String id) {
+		Id = id;
+	}
+	
+	public String getNombre() {
+		return Nombre;
+	}
+	
+	public void setNombre(String nombre) {
+		Nombre = nombre;
+	}
+
+	public String getDescripcion() {
+		return Descripcion;
+	}
+	
+	public void setDescripcion(String descripcion) {
+		Descripcion = descripcion;
+	}
 
 }
--- src/main/java/com/cenfotec/exam/web/FincaController.java
+++ src/main/java/com/cenfotec/exam/web/FincaController.java
@@ -2,11 +2,40 @@ package com.cenfotec.exam.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.cenfotec.exam.domain.Finca;
 import com.cenfotec.exam.repository.FincaRepository;
 
 @Controller
 public class FincaController {
 	@Autowired
-	private FincaRepository repository;
+	FincaRepository fincaRepository;
+	/**
+	 * Index
+	 */
+	@RequestMapping("/")
+	public String index() {
+		return "index";
+	}
+	/**
+	 * Get the Form View
+	 */
+	@GetMapping("/finca")
+	public String formFinca(Model model) {
+        model.addAttribute("finca", new Finca());
+		return "formFinca";
+	}
+	/**
+	 * Capture the submit action and store the data in the repository
+	 */
+	@PostMapping("/finca")
+	public String postFinca(@ModelAttribute Finca finca) {
+		fincaRepository.save(finca);
+		return "submittedFinca";
+	}
 }
