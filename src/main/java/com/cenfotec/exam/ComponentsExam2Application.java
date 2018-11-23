10eef4838d8923b0f57c930ddb0db94d300fb471
Gary Valverde Hampton <gvalverde@outcoding.com>
2018-11-22 20:43:12 -0600

    initial commit

---
+++ src/main/java/com/cenfotec/exam/ComponentsExam2Application.java
@@ -0,0 +1,12 @@
+package com.cenfotec.exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComponentsExam2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComponentsExam2Application.class, args);
+	}
+}
